a79bef1634f72d8783f38e2d2036e8dafa29fa7f
Jazzel Mehmood <jazzelmehmood6@gmail.com>
2020-07-18 00:23:28 +0500

    Site done - Animations left

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
 import { Body } from "./components/Body";
 import "./App.css";
 
@@ -13,9 +12,6 @@ function App() {
       <section>
         <Body />
       </section>
-      <footer>
-        <Footer />
-      </footer>
     </div>
   );
 }
